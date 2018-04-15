1256fc62f61e9eee6fad5f0d19eb3f22e91b254c
duquedd1@gmail.com Duque <duquedd1@gmail.com>
2018-04-15 16:41:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function deleteFromObjectByKey(object,key){
   delete object[key]
   return object
 }
+function destructivelyDeleteFromObjectByKey(object,key){
+  delete object[key]
+  return object
+}
